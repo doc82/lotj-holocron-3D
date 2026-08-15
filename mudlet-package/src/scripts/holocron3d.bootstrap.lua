bd145993cd59ed63d0d31559c2b05d08e7cacafb
Bruce Bjorklund <bjorklund.bruce@gmail.com>
2026-08-15 12:13:37 -0700

    chore: release 0.1.5

--- mudlet-package/src/scripts/holocron3d.bootstrap.lua
+++ mudlet-package/src/scripts/holocron3d.bootstrap.lua
@@ -1,7 +1,7 @@
 lotjHolocron3DPackage = lotjHolocron3DPackage or {}
 
 local Package = lotjHolocron3DPackage
-Package.VERSION = "0.1.4"
+Package.VERSION = "0.1.5"
 Package.root = getMudletHomeDir() .. "/Holocron3D"
 Package.devConfigPath = getMudletHomeDir() .. "/holocron3d-dev-app-path.txt"
 Package.settingsPath = getMudletHomeDir() .. "/holocron3d-settings.txt"
